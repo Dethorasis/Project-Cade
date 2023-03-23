bf9ecde2166ad5789310ebf1954ed6932631058c
cade <cadefitchett1@gmail.com>
2023-03-23 17:36:55 +1300

    added basic moving functionality for ellipse

--- client/components/sketch.js
+++ client/components/sketch.js
@@ -1,14 +1,30 @@
 function sketch(p) {
+  let xPos = p.width / 2
+  let yPos = p.height / 2
+  let speed = 10
+
   p.setup = function () {
-    p.createCanvas(1800, 900)
+    p.createCanvas(1600, 800)
     p.strokeWeight(4)
     p.rect(0, 0, p.width, p.height)
   }
 
   p.draw = function () {
-    p.background(0)
+    p.background(105, 105, 105)
+    p.ellipse(xPos, yPos, 40)
 
-    p.ellipse(p.mouseX, p.mouseY, 30, 30)
+    if (p.keyIsDown(p.LEFT_ARROW)) {
+      xPos -= speed
+    }
+    if (p.keyIsDown(p.RIGHT_ARROW)) {
+      xPos += speed
+    }
+    if (p.keyIsDown(p.DOWN_ARROW)) {
+      yPos += speed
+    }
+    if (p.keyIsDown(p.UP_ARROW)) {
+      yPos -= speed
+    }
   }
 }
 
